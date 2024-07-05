6b3cf3077342e517f27b686e24452ec9742e403d
Infinityy <120841985+Inf1nityy@users.noreply.github.com>
2024-07-05 17:09:40 +0200

    match indicator opacity with crosshair opacity

--- src/main/java/me/infinityy/crosshairindicator/CrosshairIndicator.java
+++ src/main/java/me/infinityy/crosshairindicator/CrosshairIndicator.java
@@ -1,10 +1,14 @@
 package me.infinityy.crosshairindicator;
 
 import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CrosshairIndicator implements ClientModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("crosshair-indicator");
+
     @Override
     public void onInitializeClient() {
-
+        LOGGER.info("Initialized crosshair indicator.");
     }
 }
--- src/main/java/me/infinityy/crosshairindicator/mixin/InGameHudMixin.java
+++ src/main/java/me/infinityy/crosshairindicator/mixin/InGameHudMixin.java
@@ -1,5 +1,7 @@
 package me.infinityy.crosshairindicator.mixin;
 
+import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
@@ -15,17 +17,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(InGameHud.class)
 public class InGameHudMixin {
-    @Final @Shadow private MinecraftClient client;
-    @Unique Identifier textureLocation = new Identifier("crosshairindicator", "crosshair.png");
+    @Shadow @Final private MinecraftClient client;
+    @Unique Identifier textureLocation = Identifier.of("crosshairindicator", "crosshair.png");
     @Unique int textureWidth = 9;
     @Unique int textureHeight = 9;
 
-    @Inject(method = "renderCrosshair", at = @At("RETURN"))
+    @Inject(method = "renderCrosshair", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawGuiTexture(Lnet/minecraft/util/Identifier;IIII)V", ordinal = 0))
     private void drawCrosshair(DrawContext context, float tickDelta, CallbackInfo ci) {
-        assert this.client.player != null;
-        if (!this.client.player.isSpectator() && this.client.targetedEntity instanceof PlayerEntity) {
-            context.drawTexture(this.textureLocation, (client.getWindow().getScaledWidth() - 9) / 2, (client.getWindow().getScaledHeight() - 9) / 2, 0, 0, this.textureWidth, this.textureHeight);
+        if (this.client.targetedEntity instanceof PlayerEntity) {
+            context.drawTexture(this.textureLocation, (context.getScaledWindowWidth() - 9) / 2, (context.getScaledWindowHeight() - 9) / 2, 0, 0, this.textureWidth, this.textureHeight);
         }
 
+        RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
+        RenderSystem.blendFuncSeparate(GlStateManager.SrcFactor.ONE_MINUS_DST_COLOR, GlStateManager.DstFactor.ONE_MINUS_SRC_COLOR, GlStateManager.SrcFactor.ONE, GlStateManager.DstFactor.ZERO);
     }
 }
